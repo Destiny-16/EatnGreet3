d9131a34348765a48038450ede98b308e272a45a
Victoria Schimansky <vschimansky@yahoo.com>
2020-04-02 16:55:48 -0400

    Completed Contact Us

--- app/src/main/java/com/example/eatngreet/ContactUs.java
+++ app/src/main/java/com/example/eatngreet/ContactUs.java
@@ -3,13 +3,16 @@ package com.example.eatngreet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class ContactUs extends AppCompatActivity implements View.OnClickListener{
-    Button button22;
-
+    Button button22, button1;
+    EditText messageName, messageSubject, messageBody;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +20,40 @@ public class ContactUs extends AppCompatActivity implements View.OnClickListener
         setContentView(R.layout.activity_contact_us);
 
         button22 = findViewById(R.id.button22);
+        button1 = findViewById(R.id.button1);
+        messageName = (EditText)findViewById(R.id.editText1);
+        messageSubject = (EditText)findViewById(R.id.editText2);
+        messageBody = (EditText)findViewById(R.id.editText3);
 
         button22.setOnClickListener(this);
+
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                // Getting information from xml file
+                String nameFrom = messageName.getText().toString();
+                String body = messageBody.getText().toString();
+
+                // Hardcoding email recipient and subject
+                String[] mailto = new String[1];
+                mailto[0] = "tori9peaches@yahoo.com";
+                String subject = "EatNGreet Issue".concat(" - ").concat(nameFrom);
+
+                // Creating email intent
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, mailto);
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+                emailIntent.putExtra(Intent.EXTRA_TEXT, body);
+                emailIntent.setType("message/rfc822");
+
+                // Launching email intent
+                startActivity(Intent.createChooser(emailIntent,"Choose Mail App"));
+
+            }
+        });
     }
+
 
     @Override
     public void onClick(View view) {
--- app/src/main/java/com/example/eatngreet/RateActivity.java
+++ app/src/main/java/com/example/eatngreet/RateActivity.java
@@ -1,6 +1,7 @@
 package com.example.eatngreet;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,37 +10,121 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.eatngreet.ui.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RateActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button button1;
     Button button2;
+    boolean exists;
+    double likes, dislikes;
+    private static final String TAG = Registration.class.getSimpleName();
+    private DatabaseReference mFirebaseDatabase;
+    private FirebaseDatabase mFirebaseInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate);
 
+        // Create buttons and listeners
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
-
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
+
+        // DO NOT CHANGE / CREATES DATABASE CONNECTION***********************************************************
+        FirebaseUser user2 = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(USER_SERVICE).child(user2.getUid());
+        String ref2 = ref.toString().trim();
+
+        FirebaseUser user3 = FirebaseAuth.getInstance().getCurrentUser();
+        final String userId = user3.getUid();
+
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        mFirebaseDatabase = mFirebaseInstance.getReference("users").child(userId);
+        // *******************************************************************************************************
+
+        // Check if the field exists, if it does then get likes and dislikes
+        mFirebaseDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.hasChild("likes")){
+                    setExists(true);
+                    setLikes((double)dataSnapshot.child("likes").getValue());
+                    setDislikes((double)dataSnapshot.child("dislikes").getValue());
+                }
+                else setExists(false);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Left Empty
+            }
+        });
+
+        // Create the new field if not exists, set likes and dislikes
+        if(getExists() == false) {
+            mFirebaseDatabase.push().setValue("likes");
+            mFirebaseDatabase.push().setValue("dislikes");
+            mFirebaseDatabase.child("likes").push().setValue(0);
+            mFirebaseDatabase.child("dislikes").push().setValue(0);
+            setLikes(0);
+            setDislikes(0);
+        }
+
+        // Increment and apply new like or dislike
         switch(view.getId()) {
 
             case R.id.button1:
+                double likes = getLikes() + 1;
+                mFirebaseDatabase.child(userId).child("likes").setValue(likes);
                 startActivity(new Intent(getApplicationContext(), MainMenu.class));
                 break;
 
             case R.id.button2:
+                double dislikes = getDislikes() + 1;
+                mFirebaseDatabase.child(userId).child("dislikes").setValue(dislikes);
                 Intent intent = new Intent(getApplicationContext(), MainMenu.class);
                 startActivity(intent);
                 break;
         }
 
     }
+
+    public void setLikes(double likesNum){
+        likes = likesNum;
+    }
+
+    public double getLikes(){
+        return likes;
+    }
+
+    public void setDislikes(double dislikesNum){
+        dislikes = dislikesNum;
+    }
+
+    public double getDislikes(){
+        return dislikes;
+    }
+
+    public void setExists(boolean bool){
+        exists = bool;
+    }
+
+    public boolean getExists(){
+        return exists;
+    }
+
 }
